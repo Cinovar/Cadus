fa09c2e667fb3b69d0c0dc3738728a68eb3b60be
Victor Lemos de Freitas <victorlemosfr@gmail.com>
2026-06-18 18:18:55 -0300

    feat: Atualiza config de database e conexao com prisma

--- apps/auth/src/infra/adapters/db.ts
+++ apps/auth/src/infra/adapters/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../database/generated/prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const adapter = new PrismaNeon({
-  connectionString: process.env.NEON_DATABASE_URL_AUTH!,
+  connectionString: process.env.DATABASE_URL_POOLED!,
 })
 
 export const prisma = new PrismaClient({ adapter })
