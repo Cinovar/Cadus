c4c6ea853f7100d580dcea7441b536f4dde4fb38
Gabriel Cassio <gabriel.cgc.contato@gmail.com>
2026-06-20 18:31:53 -0300

    chore: Modificacao de posicao no repositorio

--- apps/register/src/application/dtos/RegisterIdentidadeDTO.ts
+++
@@ -1,4 +0,0 @@
-import type { Identidade } from "../../domain/entities/identidade/Identidade"
-import type { Either } from "../../shared/Either"
-
-export type RegisterIdentidadeDTO = Either<Error | Error[], Identidade>;
