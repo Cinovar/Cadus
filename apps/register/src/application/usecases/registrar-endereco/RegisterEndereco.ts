a36c6b8aa994dab94b2b10ecfd7b07814559fe9c
Gabriel Cassio <gabriel.cgc.contato@gmail.com>
2026-06-20 18:23:00 -0300

    fix: Relacionando as entidades Endereco e Identidade

--- apps/register/src/application/usecases/registrar-endereco/RegisterEndereco.ts
+++ apps/register/src/application/usecases/registrar-endereco/RegisterEndereco.ts
@@ -1,17 +1,17 @@
-import type { EnderecoDados } from "../../domain/entities/endereco/EnderecoDados";
-import { IdentidadeId } from "../../domain/entities/identidade/IdentidadeId";
-import { CryptoIdGenerator } from "../../infra/adapters/CryptoIdGenerator";
-import type { RegisterEnderecoDTO } from "../dtos/RegisterEnderecoDTO";
-import type { IEnderecoRespository } from "../ports/EnderecoRepository";
-import type { IRegisterEnderecoUC } from "../ports/RegisterEnderecoUC";
+import type { EnderecoDados } from "../../../domain/entities/endereco/EnderecoDados";
+import { IdentidadeId } from "../../../domain/entities/identidade/IdentidadeId";
+import { CryptoIdGenerator } from "../../../infra/providers/CryptoIdGenerator";
+import type { RegisterEnderecoDTO } from "../../ports/dtos/RegisterEnderecoDTO";
+import type { IEnderecoRespository } from "../../ports/respositories/IEnderecoRepository";
+import type { IRegisterEnderecoUC } from "../../ports/IRegisterEnderecoUC";
 
 // Utils
-import { type Either, success, failure } from "../../shared/Either";
+import { type Either, success, failure } from "../../../shared/Either";
 
 
-import { EnderecoId } from "../../domain/entities/endereco/EnderecoId";
-import { Endereco } from "../../domain/entities/endereco/Endereco";
-import { EnderecoAlreadyExists } from "../errors/RegisterEnderecoError";
+import { EnderecoId } from "../../../domain/entities/endereco/EnderecoId";
+import { Endereco } from "../../../domain/entities/endereco/Endereco";
+import { EnderecoAlreadyExists } from "../../errors/RegisterEnderecoError";
 
 export class RegisterEndereco implements IRegisterEnderecoUC {
     private readonly _enderecoRepo: IEnderecoRespository;
@@ -35,7 +35,7 @@ export class RegisterEndereco implements IRegisterEnderecoUC {
             return failure(new EnderecoAlreadyExists("O endereco ja existe."));
         }
         else {
-            await this._enderecoRepo.add(endereco.value);
+            await this._enderecoRepo.add(endereco);
         }
 
         return success(endereco);
