4d3a16f4258db3e0fe4745d232a3f295c8d65a5a
Victor Lemos de Freitas <victorlemosfr@gmail.com>
2026-06-23 16:23:24 -0300

    chore: adicionar script dev e remover NeonDbAdapter inutilizado

--- apps/register/src/infra/adapters/NeonDbAdapter.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import { PrismaClient } from '../database/generated/prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-
-const adapter = new PrismaNeon({
-  connectionString: process.env.NEON_DATABASE_URL_DEV!,
-})
-
-export const prisma = new PrismaClient({ adapter })
